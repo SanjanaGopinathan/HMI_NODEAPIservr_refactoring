d52d9dc889661461be7a3ce7f92a925054613637
Sanjana-ecrio <sanjana@ecrio.com>
2026-03-05 17:50:40 -0500

    port and python package update

--- web_app/src/lib/api.ts
+++ web_app/src/lib/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 // Orchestrator API (Workflows, Queries)
-//export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
-export const API_URL = 'http://localhost:8000';
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8020';
 
 export const api = axios.create({
     baseURL: API_URL,
